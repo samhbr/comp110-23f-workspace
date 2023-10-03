3bc85b6491bbdef08d04b448561b1ba3732a09b5
Samantha Breen <131218624+samb117@users.noreply.github.com>
2023-10-02 22:29:18 -0400

    finished!

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -7,11 +7,13 @@ def contains_char(secret_string: str, guess_string: str) -> bool:
     assert len(guess_string) == 1
     char_present = False
     index = 0
+    
     # Checks if the letter in the guess is at each position in the secret word.
     while index < len(secret_string):
         if guess_string == secret_string[index]:
             char_present = True
         index = index + 1
+    
     # Returns the outcome of searching for the character to the function.
     if char_present is False: 
         return False 
@@ -23,9 +25,11 @@ def emojified(guess_string: str, secret_string: str) -> str:
     WHITE_BOX = "\U00002B1C"
     GREEN_BOX = "\U0001F7E9"
     YELLOW_BOX = "\U0001F7E8"
-    assert len(guess_string) == len(secret_string)
+    assert len(guess_string) == len(secret_string
+    )
     emoji_result = ""
     secret_index = 0
+
     while secret_index < len(secret_string):
         if guess_string[secret_index] == secret_string[secret_index]:
             emoji_result += GREEN_BOX
@@ -40,15 +44,17 @@ def emojified(guess_string: str, secret_string: str) -> str:
 def input_guess(len_of_wordle: int) -> str: 
     """Lets the user know if the chosen word is the length of the secret word."""
     users_guess = input(f"Enter a {len_of_wordle} character word: ")
+
     while len(users_guess) != len_of_wordle: 
         users_guess = input(f"That wasn't {len_of_wordle} chars! Try again: ")
     return users_guess
 
 def main() -> None: 
-    """The entrypoint of the program and main game loop"""
+    """The entrypoint of the program and main game loop."""
     secret_word = "codes"
     turns = 1
     win = False
+
     while turns < 7 and win is False: 
         print(f"=== Turn {turns}/6 ===")
         guess_of_wordle = input_guess(len(secret_word))
