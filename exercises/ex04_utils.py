f1a7284c792373ff4fb55e8cf8160bbbd24d21c8
Samantha Breen <131218624+samb117@users.noreply.github.com>
2023-10-09 21:26:11 -0400

    finished ex04!

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,4 +1,4 @@
-"""EX04 - Exercise using 'List' Utility Functions"""
+"""EX04 - Exercise using 'List' Utility Functions."""
 
 __author__ = "730563340"
 
@@ -6,12 +6,12 @@ def all(num_list, given_int) -> bool:
     """Check if all elements in num_list are equal to given_int."""
     num_idx = 0  # Initialize the index
 
-    # Check if num_list is empty, return False
+    # Check if num_list is empty, return False.
     if len(num_list) == 0:
         return False
 
     while num_idx < len(num_list):
-        if num_list[num_idx] != given_int: 
+        if num_list[num_idx] != given_int:
             return False
         num_idx += 1
 
@@ -23,7 +23,7 @@ def max(input: list[int]) -> int:
         raise ValueError("max() arg is an empty List")
 
     max_num = input[0]  # Initialize max_num with the first element of the list
-    num_idx = 1  # Begin comparison from the second element
+    num_idx = 1  # Begin comparison from the second element.
 
     while num_idx < len(input):
         if input[num_idx] > max_num:
@@ -32,7 +32,7 @@ def max(input: list[int]) -> int:
 
     return max_num
 
-def is_equal(list1: list[int], list2: list[int]) -> bool:
+def is_equal(list1, list2) -> bool:
     """Check if two lists of integers are equal at each index."""
     if len(list1) != len(list2):
         return False
