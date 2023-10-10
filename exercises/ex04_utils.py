34a9a29b5b86a2a0b774254eb5a97bcf0a9c8e2c
Samantha Breen <131218624+samb117@users.noreply.github.com>
2023-10-09 21:12:19 -0400

    finished

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,20 +2,21 @@
 
 __author__ = "730563340"
 
-def all(num_list: list[int], given_int: int) -> bool:
-    if len(num_list) == 0:
-        return False
-    
-    for num in num_list:
-        if num != given_int:
+def all(num_list, given_int) -> bool:
+    """Check if all elements in num_list are equal to given_int."""
+    num_idx: int = 0 
+    while num_idx < len(num_list):
+        if num_list[num_idx] != given_int: 
             return False
-    
-    return True
+        num_idx += 1
+        if len(num_list) == 0: 
+            return False
+        return True
 
 def max(input: list[int]) -> int:
+    """Find the maximum integer in the input list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
-    
     max_num = input[0]  # Initialize max_num with the first element of the list
     num_idx = 1  # Begin comparison from second element
     
@@ -27,6 +28,7 @@ def max(input: list[int]) -> int:
     return max_num
 
 def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Check if two lists of integers are equal at each index."""
     if len(list1) != len(list2):
         return False
 
