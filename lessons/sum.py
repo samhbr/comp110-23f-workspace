7c2330db1a12ccd417eb3a194fc05a4aeab26edd
Samantha Breen <131218624+samb117@users.noreply.github.com>
2023-10-17 14:39:24 -0400

    finished

--- lessons/sum.py
+++ lessons/sum.py
@@ -1,22 +1,25 @@
 """Summing the elements of a list using different loops!"""
 __author__ = "730563340"
 
-def w_sum(vals: list[float]) -> float: 
+def w_sum(vals: list[float]) -> float:
+    """Sum using a while loop."""
     idx: int = 0
-    sum: float = 0.0
-    while idx < len(vals): 
-        sum += w_sum[idx]
-        idx += 1 
-    return sum 
+    total_sum: float = 0.0
+    while idx < len(vals):
+        total_sum += vals[idx]
+        idx += 1
+    return total_sum
 
-def f_sum(vals: list[float]) -> float: 
-    sum: float = 0.0 
+def f_sum(vals: list[float]) -> float:
+    """Sum using a for loop."""
+    total_sum: float = 0.0
     for elem in vals:
-        sum += elem  
-    return sum 
+        total_sum += elem
+    return total_sum
 
-def f_range_sum(vals: list[float]) -> float: 
-    sum: float = 0.0
-    for idx in range (0,len(f_range_sum)):
-        sum += vals[idx]
-    return sum 
+def f_range_sum(vals: list[float]) -> float:
+    """Sum using a for loop with range."""
+    total_sum: float = 0.0
+    for idx in range(len(vals)):
+        total_sum += vals[idx]
+    return total_sum
