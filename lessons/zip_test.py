f24367780049fc7959f3dc0be0970e04ae5ce457
Samantha Breen <131218624+samb117@users.noreply.github.com>
2023-10-27 20:48:31 -0400

    finished

--- lessons/zip_test.py
+++ lessons/zip_test.py
@@ -10,14 +10,16 @@ def test_empty_dict() -> None:
     second_list: list[int] = [4, 8, 1]
     assert zip[first_list, second_list] == {}
 
+
 def test_zip_dictionary_with_letters():
     """Test if the zip function returns a dictionary when provided with lists of single letters."""
-    first_list = ["z","p", "f"]
-    second_list = [4, 8, 1]
-    assert zip(first_list, second_list) == {"z": 9, "p": 2, "f": 4}
+    first_list = list[str] = ["z","p", "f"]
+    second_list = list[int] = [4, 8, 1]
+    assert zip(first_list, second_list) == {"z": 4, "p": 8, "f": 1}
+
 
 def test_zip_dictionary_with_words():
     """Test if the zip function correctly pairs words and integers into a dictionary."""
-    first_list = ["taco", "pizza", "banana"]
-    second_list = [1234, 12345, 123456]
+    first_list = list[str] = ["taco", "pizza", "banana"]
+    second_list = list[int] = [1234, 12345, 123456]
     assert zip(first_list, second_list) == {"taco": 1234, "pizza": 12345, "banana": 123456}
